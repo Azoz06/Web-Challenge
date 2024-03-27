c069b042f795913684a828e9ad60390562f5815f
Abdulaziz Al Ahmari <156913028+Azoz06@users.noreply.github.com>
2024-03-28 00:54:15 +0300

    Project files have been uploaded
    The challenge project files have been uploaded from the experiment to the final version, except for the README file.

---
+++ home.js
@@ -0,0 +1,11 @@
+// const images = document.querySelectorAll('.test-img');
+
+// images.forEach(img => {
+//   img.addEventListener('mouseover', () => {
+//     img.style.transform = 'translateX(100px)';
+//   });
+
+//   img.addEventListener('mouseout', () => {
+//     img.style.transform = 'translateX(0)';
+//   });
+// });
---
+++ home.py
@@ -0,0 +1,47 @@
+      <!-- cards -->
+      <div class="serv">
+        <h2>أرقام مُفيد</h2>
+      </div>
+
+      <!-- card No.1 -->
+      <div class="c1-img">
+        <img src="../img/member_icon.svg" alt="user img" />
+      </div>
+      <div class="card1">
+        <h2 class="cp1">عدد مستخدمين المنصة</h2>
+        <h1 class="nuc1">+<span id="counter1">1000</span></h1>
+        <span class="">مستخدم</span>
+      </div>
+
+    <!-- card No.2 -->
+
+      <div class="c1-img">
+        <img src="../img/member_icon.svg" alt="user img" />
+      </div>
+      <div class="card1">
+        <h2 class="cp1">عدد إشتراكات المنصة</h2>
+        <h1 class="nuc1">+<span id="counter1">8000</span></h1>
+        <span class="">إشتراك</span>
+      </div>
+
+          <!-- card No.3 -->
+          
+      <div class="c1-img">
+        <img src="../img/rocket_icon.svg" alt="user img" />
+      </div>
+      <div class="card1">
+        <h2 class="cp1">عدد الملفات المروفعة على المنصة</h2>
+        <h1 class="nuc1">+<span id="counter1">800</span></h1>
+        <span class="">ملف</span>
+      </div>
+
+          <!-- card No.4 -->
+
+      <div class="c1-img">
+        <img src="../img/registerd_icon.svg" alt="user img" />
+      </div>
+      <div class="card1">
+        <h2 class="cp1">عدد الشركاء</h2>
+        <h1 class="nuc1">+<span id="counter1">20</span></h1>
+        <span class="">شريك</span>
+      </div>
